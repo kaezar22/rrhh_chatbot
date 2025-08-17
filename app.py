42bf2244fce029756b7d039f1071b62a9e03449c
kaezar22 <106036350+kaezar22@users.noreply.github.com>
2025-08-17 12:27:21 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from utils.loader import load_files
+from utils.vectorstore import create_vectorstore
+from utils.llm import ask_deepseek
+
+# 👉 Esto debe ir al inicio del archivo, antes de st.title()
+st.set_page_config(
+    page_title="Caro Answers",
+    page_icon="💬",
+    layout="wide"
+)
+st.title("📄 Carolina-Bot")
+
+# Inicializar
+FILE_PATHS = ["data/reglamento.pdf", "data/recursos_humanos.txt"]
+
+if "vectorstore" not in st.session_state:
+    st.write("🔄 Cargando documentos...")
+    docs = load_files(FILE_PATHS)
+    st.session_state.vectorstore = create_vectorstore(docs)
+
+
+# Entrada usuario
+question = st.text_input("Qué duda tienes?")
+
+if question:
+    retriever = st.session_state.vectorstore.as_retriever(search_kwargs={"k": 3})
+    related_docs = retriever.get_relevant_documents(question)
+
+    context = "\n\n".join([d.page_content for d in related_docs])
+    answer = ask_deepseek(question, context)
+
+    st.subheader("Respuesta:")
+    st.write(answer)
+
+    with st.expander("📚 Contexto usado"):
+        st.write(context)
---
+++ config.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+DEEPSEEK_API_KEY = st.secrets.get("DEEPSEEK_API_KEY", "")
