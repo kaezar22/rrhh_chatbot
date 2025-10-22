a73d57e77e777ca46f265ed35e20d93679bfd5f4
kaezar22 <106036350+kaezar22@users.noreply.github.com>
2025-10-22 09:45:55 -0500

    Update vectorstore.py

--- utils/vectorstore.py
+++ utils/vectorstore.py
@@ -7,10 +7,14 @@ def create_vectorstore(docs):
     chunks = splitter.split_documents(docs)
 
     # Modelo de embeddings ligero y rápido
-    embeddings = HuggingFaceEmbeddings( model_name="sentence-transformers/all-MiniLM-L6-v2", encode_kwargs={"device": "cpu"})
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={"device_map": None},
+        encode_kwargs={"device": "cpu"})
 
     vectorstore = FAISS.from_documents(chunks, embeddings)
     return vectorstore
+
 
 
 
