adcb5ed981be0f7066f92d67f36b6d1a22717fa1
kaezar22 <106036350+kaezar22@users.noreply.github.com>
2025-10-22 09:38:15 -0500

    Update vectorstore.py

--- utils/vectorstore.py
+++ utils/vectorstore.py
@@ -1,4 +1,4 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.text_splitters import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 
@@ -11,5 +11,6 @@ def create_vectorstore(docs):
 
     vectorstore = FAISS.from_documents(chunks, embeddings)
     return vectorstore
+
 
 
