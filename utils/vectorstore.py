69d9834a9bd1c58716fdbe424bf08603a6a85805
kaezar22 <106036350+kaezar22@users.noreply.github.com>
2025-10-22 09:41:08 -0500

    Update vectorstore.py

--- utils/vectorstore.py
+++ utils/vectorstore.py
@@ -1,6 +1,6 @@
-from langchain.text_splitters import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
 def create_vectorstore(docs):
     splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
@@ -11,6 +11,7 @@ def create_vectorstore(docs):
 
     vectorstore = FAISS.from_documents(chunks, embeddings)
     return vectorstore
+
 
 
 
